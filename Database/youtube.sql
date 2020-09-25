7ad07afcd5a33c60077adbdba29e76ddc3cea96a
Fügedi Csaba <fugedic@gmail.com>
2020-09-25 06:47:23 +0200

    Youtube table sql
    This document contains the Youtube table create sql commands

---
+++ Database/youtube.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `Youtube` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `full_name` varchar(200),
+  `picture_link` varchar(255),
+  `decritption` varchar(500),
+  `author` varchar(200),
+  `rate` int,
+  `category` varchar(255)
+);
