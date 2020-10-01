3c73138814db1077f9cffcfc3601e29cb132d4c7
Nagy Zoltan <zolya1999@gmail.com>
2020-10-01 10:40:57 +0200

    Mini Project folder created and index.php uploaded

---
+++ Mini Project/index.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+<?php require_once 'protected/config.php'; ?>
+<?php require_once PROTECTED_DIR.'header.php'; ?>
+<?php require_once USER_MANAGER; ?>
+<?php require_once PROTECTED_DIR.'navbar.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+<body>
+      
+</body>
+</html>
