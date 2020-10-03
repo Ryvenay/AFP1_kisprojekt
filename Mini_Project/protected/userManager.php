b7e19c63c21a555f458b4b3d3c51dd8866aefe77
Nagy Zoltan <zolya1999@gmail.com>
2020-10-03 17:31:10 +0200

    Sha1 classification removed

--- Mini_Project/protected/userManager.php
+++ Mini_Project/protected/userManager.php
@@ -16,7 +16,7 @@ function UserLogin($username, $password)
 	$query = "SELECT id, username FROM users WHERE username = :username AND password = :password";
 	$params =
 	[ ':username' => $username,
-	  ':password' => sha1($password)];
+	  ':password' => $password];
 	require_once DATABASE_CONTROLLER;
 	$record = getRecord($query, $params);
 	if(!empty($record))
