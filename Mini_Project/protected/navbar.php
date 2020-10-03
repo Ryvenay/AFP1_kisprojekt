d4a6a2923da848dd28ae6bb7ce4babeeaea92d62
Nagy Zoltan <zolya1999@gmail.com>
2020-10-03 10:36:58 +0200

    navbar links extended

--- Mini_Project/protected/navbar.php
+++ Mini_Project/protected/navbar.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <a class="navbar-brand" href="#">Home</a>
+  <a class="navbar-brand" href="index.php?P=home">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Reviews<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php?P=reviews">Reviews<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Interviews</a>
+        <a class="nav-link" href="index.php?P=interviews">Interviews</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Other</a>
+        <a class="nav-link" href="index.php?P=other">Other</a>
       </li>
       <li class="nav-item">
         <a class="nav-link disabled" href="#"></a>
