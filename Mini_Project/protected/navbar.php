b29d4e34a24d535aa0144235e8162add265c0e88
Nagy Zoltan <zolya1999@gmail.com>
2020-10-03 10:11:40 +0200

    navbar.php edited

--- Mini_Project/protected/navbar.php
+++ Mini_Project/protected/navbar.php
@@ -1,1 +1,23 @@
-Navbar
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand" href="#">Login</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Albums<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Interviews</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pictures</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div style="margin-left:10%">
