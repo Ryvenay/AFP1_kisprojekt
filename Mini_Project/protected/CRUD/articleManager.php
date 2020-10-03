179a230937f3f674368dad7ce11a10b3828faef3
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-10-03 10:00:14 +0200

    getLatestArticles and getArticlesByCategory added
    etLatestArticles and getArticlesByCategory added to the article manager

--- Mini_Project/protected/CRUD/articleManager.php
+++ Mini_Project/protected/CRUD/articleManager.php
@@ -4,7 +4,37 @@
         $params = [ ':id' => $id ];
 
         require_once DATABASE_CONTROLLER;
-        record = getRecord($query, $params);
+        $record = getRecord($query, $params);
+
+        if (empty($record)) {
+            return false;
+        }
+        else {
+            return $record;
+        }
+    }
+
+    function getLatestArticles($category) {
+        $query = "SELECT * FROM articles WHERE category = :category ORDER BY create_date LIMIT 4";
+        $params = [ ':category' => $category ];
+
+        require_once DATABASE_CONTROLLER;
+        $record = getRecord($query, $params);
+
+        if (empty($record)) {
+            return false;
+        }
+        else {
+            return $record;
+        }
+    }
+
+    function getArticlesByCategory($category) {
+        $query = "SELECT * FROM articles WHERE category = :category";
+        $params = [ ':category' => $category ];
+
+        require_once DATABASE_CONTROLLER;
+        $record = getRecord($query, $params);
 
         if (empty($record)) {
             return false;
