e9aeaf8abea127323bcc6b83f4ae5562fa0ed58c
Nagy Zoltan <zolya1999@gmail.com>
2020-10-03 14:39:19 +0200

    home.php edited, early view

--- Mini_Project/protected/normal/home.php
+++ Mini_Project/protected/normal/home.php
@@ -5,3 +5,16 @@
     $latestInterviews = getLatestArticles('interview');
     $latestOther = getLatestArticles('other');
 ?>
+
+<?php foreach($latestReviews as $a) : ?>
+				<div class="container">
+					<div class="card" style="width: 60rem;">
+						<img class="card-img-top" src="<?=$a['banner']?>" alt="Card image cap">
+						<div class="card-body">
+						<p><?=$a['title']?></p>
+						<p class="card-text"><?=$a['content']?></p>
+						<p><?=$a['author']?></p>
+						</div>
+					</div>
+				</div>
+<?php endforeach; ?>
