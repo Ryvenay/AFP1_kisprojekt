c570f2211885abfbf62cb6416cb73ce8aaa9f44d
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-10-03 14:58:24 +0200

    home.php card formatting fixes

--- Mini_Project/protected/normal/home.php
+++ Mini_Project/protected/normal/home.php
@@ -6,11 +6,11 @@
     $latestOther = getLatestArticles('other');
 ?>
 <div class="container">
-<h2>Latest reviews</h2>
+    <h2>Latest reviews</h2>
     <div class = "row">
         <?php foreach($latestReviews as $a) : ?>
-            <div class = "col-md-3">
-                <div class="card" style="width: 18rem;">
+            <div class = "col col-md-3 ">
+                <div class="card" style="width: 15rem;">
                     <img class="card-img-top" src="<?=$a['banner']?>" alt="Card image cap">
                     <div class="card-body">
                     <h5 class = "card-title"><?=$a['title']?></h5>
