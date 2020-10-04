a1055a899747166ecdbfc7dc5bca890b4621314b
Nagy Zoltan <zolya1999@gmail.com>
2020-10-04 18:16:25 +0200

    Modified reviews.php, interviews.php and other.php

--- Mini_Project/protected/normal/reviews.php
+++ Mini_Project/protected/normal/reviews.php
@@ -4,22 +4,22 @@
 ?>
 
 <h1>Reviews</h1>
-			<div class = "container">
-			<?php foreach($articles as $a) : ?>
-			<a href="index.php?P=article&A=<?=$a['id']; ?>">
-					<div class="row">
-						<div class="col-md-7">
-							<img src="<?=$a['banner']?>" alt="Card image cap">
-						</div>
-						<div class="row">
-							<ol>
-								<h5><?=$a['title']?></h5>
-								<p><?php echo strip_tags(substr($a['content'], 0, 150));?></p>
-							</ol>
-						</div>
-					</div>
-					<br>
-					<?php endforeach; ?>
-                    </div>
-                </a>
+<div class = "container">
+    <?php foreach($articles as $a) : ?>
+        <a href="index.php?P=article&A=<?=$a['id']; ?>">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?=$a['banner']?>" alt="Card image cap">
+                </div>
+                <div class="col-md-8">
+                    <ol>
+                        <h5><?=$a['title']?></h5>
+                        <p><?php echo strip_tags(substr($a['content'], 0, 150));?></p>
+                    </ol>
+                </div>
             </div>
+            <br>
+            </div>
+        </a>
+    <?php endforeach; ?>
+</div>
