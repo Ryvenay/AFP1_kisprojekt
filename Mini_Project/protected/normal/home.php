35863fb8d4601796eca084cb6daa9064271eb141
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-10-04 17:22:55 +0200

    Fixed carousel slide and controls
    controls are now working for carousel

--- Mini_Project/protected/normal/home.php
+++ Mini_Project/protected/normal/home.php
@@ -14,27 +14,29 @@
 
 <div id="carousel" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel" data-slide-to="1"></li>
+    <li data-target="#carousel" data-slide-to="2"></li>
   </ol>
   
   <div class="carousel-inner">
+      <?php $first = true; ?>
 	<?php foreach ($featured as $f): ?>
-    <div class="carousel-item active">
+    <div class="carousel-item <?=($first==true ? "active" : "") ?>">
       <img src="<?=$f['banner']?>" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
+        <h5><?=$f['title']; ?></h5>
       </div>
     </div>
+    <?php $first = false; ?>
 	<?php endforeach;?>
   </div>
   
-  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
