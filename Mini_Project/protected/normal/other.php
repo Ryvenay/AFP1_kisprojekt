720912dfaf1ec79443b92dd4a5ec793db034187b
Nagy Zoltan <zolya1999@gmail.com>
2020-10-04 18:07:38 +0200

    Extended other.php

--- Mini_Project/protected/normal/other.php
+++ Mini_Project/protected/normal/other.php
@@ -3,3 +3,24 @@
     $articles = getArticlesByCategory('Other');
 
 ?>
+
+<h1>Other</h1>
+<div class = "container">
+			<?php foreach($articles as $a) : ?>
+			<a href="index.php?P=article&A=<?=$a['id']; ?>">
+					<div class="row">
+						<div class="col-md-15">
+							<img src="<?=$a['banner']?>" alt="Card image cap">
+						</div>
+						<div class="row">
+							<ol>
+								<h5><?=$a['title']?></h5>
+								<p><?php echo strip_tags(substr($a['content'], 0, 300));?></p>
+							</ol>
+						</div>
+					</div>
+					<br>
+					<?php endforeach; ?>
+                    </div>
+                </a>
+            </div>
