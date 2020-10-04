aa094c3d57a49ef50a089b2ee8a4b9228f6fe0d0
Nagy Zoltan <zolya1999@gmail.com>
2020-10-04 15:39:26 +0200

    Carousel bugfixing in process

--- Mini_Project/protected/normal/home.php
+++ Mini_Project/protected/normal/home.php
@@ -12,10 +12,33 @@
     }
 ?>
 
-<?php 
-    echo($featured[0]['title']);
-
-?>
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  
+  <div class="carousel-inner">
+	<?php foreach ($featured as $f): ?>
+    <div class="carousel-item active">
+      <img src="<?=$f['banner']?>" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+      </div>
+    </div>
+	<?php endforeach;?>
+  </div>
+  
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 
 <div class="container">
