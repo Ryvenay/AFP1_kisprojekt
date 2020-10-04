c69bd1bfff768da4c0f2cf29e40cc9512db17987
Nagy Zoltan <zolya1999@gmail.com>
2020-10-04 15:07:57 +0200

    Early version of carousel, $feartured renamed to $featured

--- Mini_Project/protected/normal/home.php
+++ Mini_Project/protected/normal/home.php
@@ -1,6 +1,6 @@
 <?php 
     require_once ARTICLE_MANAGER;
-    $feartured = getFeaturedArticles();
+    $featured = getFeaturedArticles();
     $latestReviews = getLatestArticles('review');
     $latestInterviews = getLatestArticles('interview');
     $latestOther = getLatestArticles('other');
@@ -11,6 +11,48 @@
         echo '<div class="alert alert-success" role="alert">Delete successfull!</div>';
     }
 ?>
+
+<div class="carousel-inner">
+	<?php foreach ($featured as $f): ?>
+    <div class="item active">
+	
+      <img src="<?=$f['banner']?>">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>LA is always so much fun!</p>
+      </div>
+	  <?php endforeach;?>
+    </div>
+	
+
+    <div class="item">
+      <img src="chicago.jpg" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="ny.jpg" alt="New York">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+  </div>
+  
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 <div class="container">
     <h2>Latest reviews</h2>
     <div class = "row">
