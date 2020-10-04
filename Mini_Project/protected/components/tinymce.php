4a682c224e6228e536f0107b802c27afa78a192f
Dombi Tibor <dombidav@gmail.com>
2020-10-04 16:12:42 +0200

    Implemented TinyMCE editor

---
+++ Mini_Project/protected/components/tinymce.php
@@ -0,0 +1,10 @@
+<script src="https://cdn.tiny.cloud/1/t8y0vwt3xjxwrasjnjy5nc1tnj2xmqjhbdd2g3413iec2uez/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+<textarea class="form-control" id="articleContent" rows="30" name="content" required><?=isset($postData) ? $postData['content'] : ""; ?></textarea>
+
+<script type="text/javascript">
+  tinymce.init({
+    selector: '#articleContent',
+    inline: false
+  });
+</script>
