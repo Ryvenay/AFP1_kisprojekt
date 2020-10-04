b64d0b3cd76611cbe9efb32513a8e8b115533ff1
Nagy Zoltan <zolya1999@gmail.com>
2020-10-04 14:33:27 +0200

    Navbar extended with login, add article and logout buttons

--- Mini_Project/protected/navbar.php
+++ Mini_Project/protected/navbar.php
@@ -3,7 +3,14 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  
   <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+		<?php if(!IsUserLoggedIn()) :?>
+			<li class="nav-item active">
+			<a class="navbar-brand" href="index.php?P=login">Login<span class="sr-only">(current)</span></a>
+			</li>
+		<?php endif; ?>
     <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="navbar-brand" href="index.php?P=reviews">Reviews<span class="sr-only">(current)</span></a>
@@ -14,9 +21,14 @@
       <li class="nav-item">
         <a class="navbar-brand" href="index.php?P=other">Other</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#"></a>
-      </li>
+	  <?php if(IsUserLoggedIn()) :?>
+		<li class="nav-item">
+			<a class="navbar-brand" href="index.php?P=add_article">Add Article</a>
+		</li>
+		<li class="nav-item active">
+			<a class="navbar-brand" href="index.php?P=logout">Logout<span class="sr-only">(current)</span></a>
+			</li>
+		<?php endif;?>
     </ul>
   </div>
 </nav>
