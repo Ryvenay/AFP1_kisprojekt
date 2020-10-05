41fc016e8fd5b4079257769e4240337f0b3faf67
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-10-05 18:46:12 +0200

    Fixed not working submit button when tinyMCE is applied
    The submit button is now working with tinyMCE

--- Mini_Project/protected/components/tinymce.php
+++ Mini_Project/protected/components/tinymce.php
@@ -1,6 +1,6 @@
 <script src="https://cdn.tiny.cloud/1/t8y0vwt3xjxwrasjnjy5nc1tnj2xmqjhbdd2g3413iec2uez/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
-<textarea class="form-control" id="articleContent" rows="30" name="content" required><?=isset($postData) ? $postData['content'] : ""; ?></textarea>
+<textarea class="form-control" id="articleContent" rows="30" name="content"><?=isset($postData) ? $postData['content'] : ""; ?></textarea>
 
 <script type="text/javascript">
   tinymce.init({
