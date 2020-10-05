26fd0936345f92daeeb1d7bd5ea1ad80c867296a
Nyeste Reka <rekabeka37@gmail.com>
2020-10-05 19:38:07 +0200

    Added a class in style.css
    "about" class has been added in style.css and about us page

--- Mini_Project/protected/normal/about_us.php
+++ Mini_Project/protected/normal/about_us.php
@@ -4,14 +4,12 @@
 	</head>
     	<title></title>
 		<body>
-			<p></p>
 
-			<p><strong>Music Addict</strong> has existed online in different forms for nearly six years. Throughout that time, we have relentlessly sought to bring innovative music to a growing audience while also fostering a space to engage with music, its fans, and its creators in a positive, creative way.</p>
+			<p class="about"><strong>Music Addict</strong> has existed online in different forms for nearly six years. Throughout that time, we have relentlessly sought to bring innovative music to a growing audience while also fostering a space to engage with music, its fans, and its creators in a positive, creative way.</p>
 
-			<p>The online environment we find ourselves in today has made music discovery more accessible than ever before. At the same time, so much amazing music is made that defies easy categorization. Inspired by this observation, it is our hope that <strong>Music Addict</strong> is able to help you explore all kinds of music out there without being constrained to genre. This is what we envision <strong>Music Addict</strong> to be: a platform for a collective of music enthusiasts, supporting, encouraging, and nurturing a liberated approach to music discovery, and straddling the whole of the musical spectrum. With the team we currently have in place and the ability to properly realize our vision, we are committed to encourage the discovery and discussion of meaningful music, regardless of genre or other limitations.</p>
+			<p class="about">The online environment we find ourselves in today has made music discovery more accessible than ever before. At the same time, so much amazing music is made that defies easy categorization. Inspired by this observation, it is our hope that <strong>Music Addict</strong> is able to help you explore all kinds of music out there without being constrained to genre. This is what we envision <strong>Music Addict</strong> to be: a platform for a collective of music enthusiasts, supporting, encouraging, and nurturing a liberated approach to music discovery, and straddling the whole of the musical spectrum. With the team we currently have in place and the ability to properly realize our vision, we are committed to encourage the discovery and discussion of meaningful music, regardless of genre or other limitations.</p>
 
-			<p>Our volunteer team is committed to sharing and discussing music through a number of mediums that you can explore below. However, we are always searching for new ways to interact with this noise that is everything to us. Thank you for being a part of this positive and inquisitive community.</p>
+			<p class="about">Our volunteer team is committed to sharing and discussing music through a number of mediums that you can explore below. However, we are always searching for new ways to interact with this noise that is everything to us. Thank you for being a part of this positive and inquisitive community.</p>
 			
-			<p></p>
 		</body>
 </html>
