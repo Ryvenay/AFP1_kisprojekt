f8c16d59428a5a3244d847a4ad1c27b7208dc7bc
Nagy Zoltan <zolya1999@gmail.com>
2020-10-05 17:38:36 +0200

    Added latest interviews and other articles to home.php

--- Mini_Project/protected/normal/home.php
+++ Mini_Project/protected/normal/home.php
@@ -64,3 +64,47 @@
     <a href="index.php?P=reviews">See more...</a>
     
 </div>
+<hr>
+<div class="container">
+    <h2>Latest interviews</h2>
+    <div class = "row">
+        <?php foreach($latestInterviews as $i) : ?>
+            <div class = "col">
+                <a href="index.php?P=article&A=<?=$i['id']; ?>">
+                    <div class="card" style="width: 15rem;">
+                        <img class="card-img-top" src="<?=$i['banner']?>" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class = "card-title"><?=$i['title']?></h5>
+                            <p class="card-text"></p>
+                            <?=$i['username']?>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <a href="index.php?P=interviews">See more...</a>
+    
+</div>
+<hr>
+<div class="container">
+    <h2>Other articles</h2>
+    <div class = "row">
+        <?php foreach($latestOther as $o) : ?>
+            <div class = "col">
+                <a href="index.php?P=article&A=<?=$o['id']; ?>">
+                    <div class="card" style="width: 15rem;">
+                        <img class="card-img-top" src="<?=$o['banner']?>" alt="Card image cap">
+                        <div class="card-body" style="width: 15rem;">
+                            <h5 class = "card-title"><?=$o['title']?></h5>
+                            <p class="card-text"></p>
+                            <?=$o['username']?>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <a href="index.php?P=other">See more...</a>
+    
+</div>
