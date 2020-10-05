053a381538a6b4a1bc67822351e8f6352c5a3d73
Fügedi Csaba <fugedic@gmail.com>
2020-10-05 21:26:40 +0200

    Reworked Navbar

--- Mini_Project/protected/navbar.php
+++ Mini_Project/protected/navbar.php
@@ -1,37 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="index.php?P=home">Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-		<?php if(!IsUserLoggedIn()) :?>
-			<li class="nav-item active">
-			<a class="navbar-brand" href="index.php?P=login">Login<span class="sr-only">(current)</span></a>
-			</li>
-		<?php endif; ?>
-    <ul class="navbar-nav">
-      <li class="nav-item active">
+	<div id="navbarformat" class="col aligne-self-start">
+		<a class="navbar-brand" href="index.php?P=home">Home</a>
         <a class="navbar-brand" href="index.php?P=reviews">Reviews<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
         <a class="navbar-brand" href="index.php?P=interviews">Interviews</a>
-      </li>
-      <li class="nav-item">
         <a class="navbar-brand" href="index.php?P=other">Other</a>
-      </li>
-      <li class="nav-item">
         <a class="navbar-brand" href="index.php?P=about_us">About us</a>
-      </li>
-	  <?php if(IsUserLoggedIn()) :?>
-		<li class="nav-item">
-			<a class="navbar-brand" href="index.php?P=addArticle">Add Article</a>
-		</li>
-		<li class="nav-item active">
-			<a class="navbar-brand" href="index.php?P=logout">Logout<span class="sr-only">(current)</span></a>
-			</li>
-		<?php endif;?>
-    </ul>
-  </div>
+	</div>
+	<div class="col aligne-self-end text-right">	
+			<?php if(!IsUserLoggedIn()) :?>
+				<a class="navbar-brand" href="index.php?P=login">Login<span class="sr-only">(current)</span></a>
+			<?php endif; ?>
+			<?php if(IsUserLoggedIn()) :?>		
+				<a class="navbar-brand" href="index.php?P=addArticle">Add Article</a>
+				<a class="navbar-brand" href="index.php?P=logout">Logout<span class="sr-only">(current)</span></a>
+			<?php endif;?>
+	</div>
 </nav>
